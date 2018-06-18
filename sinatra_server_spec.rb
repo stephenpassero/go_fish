d4df621b40ff7361def461696ff87851a5b1258b
Stephen Passero <stephen.passero@gmail.com>
2018-06-18 15:50:06 -0400

    Add more tests

--- sinatra_server_spec.rb
+++ sinatra_server_spec.rb
@@ -1,12 +1,19 @@
 require 'capybara/rspec'
 require 'sinatra'
-require 'app.rb'
+require_relative 'app.rb'
 
 Capybara.app = Sinatra::Application
-set(:show_exceptions, false)
 
-describe "app", {} do
-  it 'should contain an input field' do
+describe "app", {:type => :feature} do
+  it 'should have a main page' do
+    visit('/')
+    expect(page).to have_content("Welcome to Go Fish!")
+  end
 
+  it 'go to a different page' do
+    visit('/')
+    fill_in('num_of_players', :with => '5')
+    click_button('Submit')
+    expect(page).to have_content("Player5")
   end
 end
